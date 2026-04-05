bb2baaa68cb6305b8430c949ef14fa27223e8ec1
Adolfo <adolfovalvid@gmail.com>
2026-04-05 17:06:06 -0500

    feat(products): se creo un componente de tarjetas para visualizar los productos

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCard } from "../../../products/components/product-card/product-card";
+import { GenderPage } from "../gender-page/gender-page";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './home-page.html',
 })
 export class HomePage { }
